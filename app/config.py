9188789d6f7751869e580d4ec23abe9c9bff455e
Matteo Vitali <trottomv@gmail.com>
2025-09-04 16:58:17 +0200

    Remediate security issues
    - Upgrade to jinja2~=3.1.0
    - Remove hardcoded secrets using os.getenv
    - Add timeout handler to requests
    - Security hardening of Dockerfile
    - Fix SSTI vulnerability on jinja2 template rendering
    - Turn on coraza waf

--- app/config.py
+++ app/config.py
@@ -10,6 +10,6 @@ DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes", "y")
 
 PUBLIC_IP_SERVICE_URL = os.getenv("PUBLIC_IP_SERVICE_URL")
 
-SUPER_SECRET_NAME = "John Ripper"  # FIXME: os.getenv("SUPER_SECRET_NAME")
+SUPER_SECRET_NAME = os.getenv("SUPER_SECRET_NAME")
 
-SUPER_SECRET_TOKEN = "5u93R53Cr3tT0k3n"  # FIXME: os.getenv("SUPER_SECRET_TOKEN")
+SUPER_SECRET_TOKEN = os.getenv("SUPER_SECRET_TOKEN")
